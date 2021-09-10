a8419613e5b3fcb08c7866ed143c876454cb0683
ogzhn <oguzhancavdar06@gmail.com>
2021-09-10 13:52:48 +0300

    create project structure

--- src/App.js
+++ src/App.js
@@ -1,40 +1,5 @@
-import { useState } from "react";
-import Contacts from "./components/contacts/Contacts";
-import Form from "./components/form/Form";
-import { addInfo, updateHandler } from "./utils/functions";
-import { ToastContainer } from "react-toastify";
-
-const initialValues = { username: "", phoneNumber: "", gender: "NO INFO!" };
-
 function App() {
-  const [info, setInfo] = useState(initialValues);
-
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    if (info?.id) {
-      updateHandler(info);
-    } else {
-      addInfo(info);
-    }
-    setInfo(initialValues);
-  };
-
-  const updateFormHandler = (item) => {
-    setInfo({...item });
-  };
-
-  return (
-    <div className="App">
-      <Form
-        className="form"
-        info={info}
-        setInfo={setInfo}
-        handleFormSubmit={handleFormSubmit}
-      />
-      <Contacts className="contacts" updateFormHandler={updateFormHandler} />
-      <ToastContainer />
-    </div>
-  );
+  return <div className="App">firecontact</div>;
 }
 
 export default App;
--- src/utils/customToasitfy.js
+++ src/utils/customToasitfy.js
@@ -1,14 +1,1 @@
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
-export const succesToastify = (msg) => {
-  toast.success(msg, {
-    position: "bottom-right",
-    autoClose: 3000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-  });
-};
