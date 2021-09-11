804704b682e343fde230d518d75b1cf72613152b
ogzhn <oguzhancavdar06@gmail.com>
2021-09-11 17:24:19 +0300

    create and implement custom useFetch() hook

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,7 +1,29 @@
 import firebase from "./firebase";
-
+import { useState, useEffect } from "react";
 
 export const addInfo = (info) => {
+  const contactRef = firebase.database().ref("contact");
+  contactRef.push(info);
+};
+
+export const useFetch = () => {
+  const [contactList, setContactList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
     const contactRef = firebase.database().ref("contact");
-    contactRef.push(info)
-}
+    contactRef.on("value", (snapshot) => {
+      const contacts = snapshot.val();
+
+      const contactArray = [];
+      for (let id in contacts) {
+        contactArray.push({ id, ...contacts[id] });
+      }
+      setContactList(contactArray);
+      setIsLoading(false);
+    });
+  }, []);
+
+  return { contactList, isLoading };
+};
